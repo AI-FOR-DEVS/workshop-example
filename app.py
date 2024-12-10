f20e1aad9d88196faf18dbc222c73f67915ffb47
Sebastian <sebastian@familieschlaak.de>
2024-12-10 10:12:07 +0100

    Add memory

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
 
 st.title("💬 T-Chat")
+
+if "memory" not in st.session_state:
+  st.session_state.memory = ConversationBufferMemory()
 
 llm = ChatOpenAI(model="gpt-4o")
 
 user_input = st.text_input("You: ")
 
 if st.button("Send"):
-    output = llm.invoke(user_input).content
+    st.session_state.memory.chat_memory.add_user_message(user_input)
+    complete_prompt = f"{user_input} \n History: {st.session_state.memory.load_memory_variables({})}"
+    output = llm.invoke(complete_prompt).content
+    st.session_state.memory.chat_memory.add_ai_message(output)
     st.write("Output:", output)
