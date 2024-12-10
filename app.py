a139e148fa0bc9bbe7d23142464e1dd59702a9ae
Sebastian <sebastian@familieschlaak.de>
2024-12-10 13:51:59 +0100

    Fix logic and integrate langsmith

--- app.py
+++ app.py
@@ -4,20 +4,19 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.schema import Document
 from retrievers import get_pinecone_retriever
 from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.tracers.context import tracing_v2_enabled
 
 def get_chain():
   llm = ChatOpenAI(model="gpt-4o", temperature=0)
 
   prompt = ChatPromptTemplate.from_messages([
-      ("system", "Du bist ein deutsche Mitarbeiter der T-Firma. Antworte auf die letzte Frage des Users in einem Satz basierend auf folgenden Kontext: {context}"),
-      ("user", "{input}"),
-      MessagesPlaceholder(variable_name="chat_history")
+      ("system", """
+        Du bist ein deutsche Mitarbeiter der T-Firma. 
+        Antworte auf die letzte Frage des Users in einem Satz basierend auf folgenden Kontext: {context}
+       """),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}")
   ])
-
-  # Print the prompt template
-  print("--------------------\nPrompt Template:")
-  print(prompt.format(context="<context>", input="<input>", chat_history=[]))
-
 
   chain = create_stuff_documents_chain(llm, prompt)
 
@@ -26,9 +25,10 @@ def get_chain():
 
 def stream_response(query, chat_history):
   chain = get_chain()
-  for chunk in chain.stream({"input": query, "chat_history": chat_history}):
-    if "answer" in chunk:
-      yield chunk["answer"]
+  with tracing_v2_enabled():
+    for chunk in chain.stream({"input": query, "chat_history": chat_history}):
+      if "answer" in chunk:
+        yield chunk["answer"]
 
 if __name__ == "__main__":
   for chunk in stream_response("Hi", []):
